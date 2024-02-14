95f6f17fc780a88381db2d543b667c368839c1c8
Rhigen <144778126+rhigen@users.noreply.github.com>
2024-02-15 02:01:51 +0800

    userside(about)

--- app/Views/user/about.php
+++ app/Views/user/about.php
@@ -20,19 +20,19 @@
     </section>
 
     <section class="ftco-about d-md-flex">
-    	<div class="one-half img" style="background-image: url(images/about.jpg);"></div>
-    	<div class="one-half ftco-animate">
-    		<div class="overlap">
-	        <div class="heading-section ftco-animate ">
-	        	<span class="subheading">Discover</span>
-	          <h2 class="mb-4">Our Story</h2>
-	        </div>
-	        <div>
-	  				<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
-	  			</div>
-  			</div>
-    	</div>
-    </section>
+				<div class="one-half img" style="background-image: url(/assets/images/hmpg4.jpg);"></div>
+					<div class="one-half ftco-animate">
+						<div class="overlap">
+							<div class="heading-section ftco-animate ">
+								<span class="subheading">Discover</span>
+								<h2 class="mb-4">Our Story</h2>
+							</div>
+						<div>
+							<p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for their.</p>
+						</div>
+					</div>
+				</div>
+			</section>
 
     <section class="ftco-section img" id="ftco-testimony" style="background-image: url(images/bg_1.jpg);"  data-stellar-background-ratio="0.5">
     	<div class="overlay"></div>
@@ -40,8 +40,10 @@
 	      <div class="row justify-content-center mb-5">
 	        <div class="col-md-7 heading-section text-center ftco-animate">
 	        	<span class="subheading">Testimony</span>
-	          <h2 class="mb-4">Customers Says</h2>
-	          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+	          <h2 class="mb-4">Customer's Feedback</h2>
+	          <p>"The greatest happiness of life is the conviction that we are loved; loved for ourselves, or rater, loved in spite of ourselves."
+				<br> -Victor Hugo
+			  </p>
 	        </div>
 	      </div>
 	    </div>
